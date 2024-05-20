9d8f7106ff7b6693df671b89b079df75892d05a2
zdziszkee <zdziszkee@gmail.com>
2024-05-20 18:40:46 +0200

    add gradle settings

---
+++ README.md
@@ -0,0 +1,3 @@
+Program nie potrzebuje dodatkowych zaleznosci.
+Niestety nie udalo mi sie go skonczyc w zadanym czasie, wiec nie ma interfejsu do manualnego testowania ale samo zadanie bylo bardzo fajne wiec postanowilem je przeslac tak czy siak.
+Aby uruchomic program nalezy go skompilowac za pomoca gradle build, a nastepnie uzyc komendy java -jar ElevatorSystem.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,20 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-}
 
+}
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.github.zdziszkee.elevatorsystem.ElevatorSystem"
+    }
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree) // OR .map { zipTree(it) }
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+}
 tasks.test {
     useJUnitPlatform()
 }
